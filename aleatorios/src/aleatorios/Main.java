67e3f4ad1116a207cfde2760550fe573365b53a8
hecto <hecto@DESKTOP-M9EJGUT>
2018-11-30 22:10:17 +0100

    Original

---
+++ aleatorios/src/aleatorios/Main.java
@@ -0,0 +1,15 @@
+package aleatorios;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int aux=0;
+
+		for (aux = 0; aux < 40; aux++) {
+			System.out.print((int) (Math.random()*25+1)+" ");
+			
+		}
+	}
+
+}
